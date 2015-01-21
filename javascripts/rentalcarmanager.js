1d3695bd27592b6078ae2580d561c8e5ae91ecb2
Rodrigo Barona <rbarona@gmail.com>
2015-01-21 22:31:44 +0000

    new JS webstep4

--- javascripts/rentalcarmanager.js
+++ javascripts/rentalcarmanager.js
@@ -158,3 +158,184 @@ jQuery(function () {
   $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+//Webstep4 
+
+function ismaxlength(obj){
+var mlength=obj.getAttribute? parseInt(obj.getAttribute("maxlength")) : ""
+if (obj.getAttribute && obj.value.length>mlength)
+obj.value=obj.value.substring(0,mlength)
+}
+</script>
+
+<script language="javajcript" type="text/javascript">
+<!--
+function open_new_window(url)
+{
+new_window = window.open(url,'window_name','toolbar=0,menubar=0,resizable=0,dependent=0,status=0,width=540,height=540,left=25,top=15')
+}
+function checkNumericField(objName,minval, maxval,comma,period,hyphen)
+{
+   var numberfield = objName;
+   if (chkNumeric(objName,minval,maxval,comma,period,hyphen) == false)
+   {     numberfield.select();
+      numberfield.focus();
+      return false;
+   }
+   else
+   {     return true;
+   }
+}
+function chkNumeric(objName,minval,maxval,comma,period,hyphen)
+{
+// only allow 0-9 be entered, plus any values passed
+// if all numbers allow commas, periods, hyphens or whatever,
+// just hard code it here and take out the passed parameters
+var checkOK = "0123456789";
+var checkStr = objName;
+var allValid = true;
+var decPoints = 0;
+var allNum = "";
+
+for (i = 0;  i < checkStr.value.length;  i++)
+{
+ch = checkStr.value.charAt(i);
+for (j = 0;  j < checkOK.length;  j++)
+if (ch == checkOK.charAt(j))
+break;
+if (j == checkOK.length)
+{
+allValid = false;
+break;
+}
+if (ch != ",")
+allNum += ch;
+}
+if (!allValid)
+{
+alertsay = "Your Credit Card Number or Expiry Date is invalid"
+alert(alertsay);
+return (false);
+}
+// set the minimum and maximum
+var chkVal = allNum;
+var prsVal = parseInt(allNum);
+if (chkVal != "" && !(prsVal >= minval && prsVal <= maxval))
+{
+alertsay = "Your Credit Card Number or Expiry Date is invalid"
+
+alert(alertsay);
+return (false);
+}
+}
+
+function ValidateEmail()
+{
+  if (document.theForm1.lastname.value == "")
+  {      alert("Last Name required.");
+      document.theForm1.lastname.focus();
+      return (false);
+  }
+  if (document.theForm1.CustomerEmail.value == "")
+  {      alert("Email Address required.");
+      document.theForm1.CustomerEmail.focus();
+      return (false);
+  }
+   var emailRegEx = /^[a-zA-Z0-9._-]*\@[a-zA-Z0-9._-]*$/;
+      if(!emailRegEx.test(theForm1.CustomerEmail.value))
+      {
+         alert("Invalid Email address");
+         return false;
+      }
+return (true);
+}
+function ValidateQuote()
+{
+  if (document.theForm.firstname.value == "")
+  {      alert("First Name required.");
+      document.theForm.firstname.focus();
+      return (false);
+  }
+  if (document.theForm.lastname.value == "")
+  {      alert("Last Name required.");
+      document.theForm.lastname.focus();
+      return (false);
+  }
+  if (document.theForm.CustomerEmail.value == "")
+  {      alert("Email Address required.");
+      document.theForm.CustomerEmail.focus();
+      return (false);
+  }
+   var emailRegEx = /^[a-zA-Z0-9._-]*\@[a-zA-Z0-9._-]*$/;
+      if(!emailRegEx.test(theForm.CustomerEmail.value))
+      {
+         alert("Invalid Email address");
+         return false;
+      }
+   if (document.theForm.Phone.value == "")
+  {      alert("Phone No required.");
+      document.theForm.Phone.focus();
+      return (false);
+  }
+return (true);
+}
+
+
+function Validate()
+{
+
+   if (document.theForm.firstname.value == "")
+  {      alert("First Name required.");
+      document.theForm.firstname.focus();
+      return (false);
+  }
+  if (document.theForm.lastname.value == "")
+  {      alert("Last Name required.");
+      document.theForm.lastname.focus();
+      return (false);
+  }
+
+    if (document.theForm.DOBYear.value == "1900")
+   {           alert("Please enter Driver's D.O.B ");
+               theForm.DOBYear.focus();
+
+               return (false);
+      }
+  if (document.theForm.License.value == "")
+  {      alert("License# required.");
+      document.theForm.License.focus();
+      return (false);
+  }
+  if (document.theForm.LicenseIssued.value == "")
+  {      alert("License Issued required.");
+      document.theForm.LicenseIssued.focus();
+      return (false);
+  }
+  if (document.theForm.address.value == "")
+  {      alert("Please enter your address  details.");
+      document.theForm.address.focus();
+      return (false);
+  }
+   if (document.theForm.city.value == "")
+  {      alert("Please enter your address details.");
+      document.theForm.city.focus();
+      return (false);
+  }
+   if (document.theForm.CustomerEmail.value == "")
+  {      alert("Email Address required.");
+      document.theForm.CustomerEmail.focus();
+      return (false);
+  }
+   if (document.theForm.Phone.value == "")
+  {      alert("Phone No required.");
+      document.theForm.Phone.focus();
+      return (false);
+  }
+   if (document.theForm.NoTravelling.value == "")
+  {      alert("No. of People Travelling required.");
+      document.theForm.NoTravelling.focus();
+      return (false);
+  }
+
+return (true);
+}  
